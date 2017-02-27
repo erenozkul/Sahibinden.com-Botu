e2acfc373582319c2f1b789b0dda7b4abf4eb757
Eren Özkul <erain.ozkul@gmail.com>
2017-02-27 11:58:39 +0300

    Update sahibinden.class.php

--- class/sahibinden.class.php
+++ class/sahibinden.class.php
@@ -24,11 +24,12 @@ class Sahibinden
      */
     static function Kategori($type = "json", $url = NULL)
     {
+        self::$data = array();
 
         if (empty($url)) {
             $open = self::Curl(self::$baseUrl);
             $items = str_get_html($open)->find("ul.categories-left-menu", 0)->find("li a[title]");
-            if(count($items) > 0) {
+            if (count($items) > 0) {
                 foreach ($items as $element) {
                     self::$data[] = array("title" => trim($element->plaintext),
                         "uri" => trim($element->href),
@@ -36,8 +37,8 @@ class Sahibinden
                     );
 
                 }
-            }else{
-                self::$data[] = array("error" => true, "url" => self::$baseUrl,"message" => "Sonuç Bulunamadı.");
+            } else {
+                self::$data[] = array("error" => true, "url" => self::$baseUrl, "message" => "Sonuç Bulunamadı.");
 
             }
         } else {
@@ -52,7 +53,7 @@ class Sahibinden
                     );
                 }
             } else {
-                self::$data[] = array("error" => true, "url" => $link,"message" => "Sonuç Bulunamadı.");
+                self::$data[] = array("error" => true, "url" => $link, "message" => "Sonuç Bulunamadı.");
 
             }
 
@@ -73,7 +74,7 @@ class Sahibinden
      */
     static function Liste($kategoriLink, $itemCount = 20, $filters, $type = "json")
     {
-
+        self::$data = array();
         $filterText = "";
         foreach ($filters as $key => $val) {
             $filterText .= "&" . $key . "=" . $val;
@@ -107,8 +108,8 @@ class Sahibinden
             foreach ($images as $image) {
                 $thumbArray[] = array("thumb" => trim($image->src));
                 $imageArray[] = array("image" => str_replace("thmb_", "", trim($image->src)));
-                $titleArray[] = array("title" => trim(explode("#",$image->alt)[0]));
-                $idArray[] = array("id" => trim(explode("#",$image->alt)[1]));
+                $titleArray[] = array("title" => trim(explode("#", $image->alt)[0]));
+                $idArray[] = array("id" => trim(explode("#", $image->alt)[1]));
             }
             foreach (@$prices as $price) {
                 $priceArray[] = array("price" => trim($price->plaintext));
@@ -134,7 +135,7 @@ class Sahibinden
             }
 
         } else {
-            self::$data[] = array("error" => true, "url" => $url,"message" => "Sonuç Bulunamadı.");
+            self::$data[] = array("error" => true, "url" => $url, "message" => "Sonuç Bulunamadı.");
         }
 
         return self::ReturnWithTypes($type);
@@ -146,99 +147,39 @@ class Sahibinden
      * İlan detaylarını listeler.
      *
      * @param null $url
-     * @return array
+     * @param json $type
+     * @return JSON,XML,Array
      */
-    static function Detay($uri = NULL,$type = "json")
+    static function Detay($uri = NULL, $type = "json")
     {
-        $url = self::$baseUrl.$uri;
-        if($uri != NULL){
+        self::$data = array();
+        $url = self::$baseUrl . $uri;
+        if ($uri != NULL) {
             $open = self::Curl($url);
+            $title = str_get_html($open)->find("div.classifiedDetailTitle h1", 0);
 
-            $title = str_get_html($open)->find("div.classifiedDetailTitle h1",0);
-            $breadCrumb = str_get_html($open)->find("div.classifiedBreadCrumb ul li");
-            $address = str_get_html($open)->find("div.classifiedInfo h2",0)->find("a");
-            $images = str_get_html($open)->find("ul.classifiedDetailThumbListPages img");
-            $movies = str_get_html($open)->find("source#mp4");
-            $infoListFields = str_get_html($open)->find("ul.classifiedInfoList li strong");
-            $infoListTexts = str_get_html($open)->find("ul.classifiedInfoList li span");
-            $price = str_get_html($open)->find("div.classifiedInfo h3",0);
-            $priceTrim = str_get_html($open)->find("div.classifiedInfo h3 a",0);
-            $description = str_get_html($open)->find("div.classifiedDescription",0);
-            $sellerName = str_get_html($open)->find("div.classifiedUserContent h5",0);
-            $sellerStore= str_get_html($open)->find("a.userClassifieds",0);
-            $sellerImg = str_get_html($open)->find("div.classifiedUserContent a img",0);
-            $sellerPhoneFields = str_get_html($open)->find("ul#phoneInfoPart li strong");
-            $sellerPhoneText = str_get_html($open)->find("ul#phoneInfoPart li span.pretty-phone-part");
-            $map = str_get_html($open)->find("div#gmap",0);
-            $propertyTitles = str_get_html($open)->find("div#classified-detail",0)->find("div.uiBox",1)->find("div.classifiedDescription",0)->find("h3");
-            $propertyCount = str_get_html($open)->find("div#classified-detail",0)->find("div.uiBox",1)->find("div.classifiedDescription",0)->find("ul");
-
-            if(count($propertyCount)>0){
-                for($p=0; $p<=count($propertyCount)-1; $p++){
-                    $propertyDetails = str_get_html($open)->find("div#classified-detail",0)->find("div.uiBox",1)->find("div.classifiedDescription",0)->find("ul",$p)->find("li.selected");
-                    $ppDetails = array();
-                    if(count($propertyDetails)>0){
-                        for($d=0; $d<=count($propertyDetails)-1; $d++){
-                            $ppDetails[] = trim($propertyDetails[$d]->plaintext);
-                        }
-                    }
-
-                    $propertyArray[] = array(trim($propertyTitles[$p]->plaintext) => $ppDetails);
-                }
-            }
-            if(count($sellerPhoneFields)>0){
-                for($f=0; $f<=count($sellerPhoneFields)-1; $f++){
-                    $phoneArray[] = array("title"=>trim($sellerPhoneFields[$f]->plaintext),"text" => trim($sellerPhoneText[$f]->plaintext));
-                }
-            }
-            if(count($infoListFields)>0){
-                for($f=0; $f<=count($infoListFields)-1; $f++){
-                    $infoArray[] = array("title" => trim($infoListFields[$f]->plaintext),"text" => trim($infoListTexts[$f]->plaintext));
-                }
-            }
-            if(count($breadCrumb)>0){
-                foreach ($breadCrumb as $bc){
-                    $breadArray[] = trim($bc->plaintext);
-                }
-            }
-            if(count($images) > 0) {
-                foreach ($images as $img) {
-                    $thumbArray[]=$img->src;
-                    $imageArray[]=str_replace("thmb_","",$img->src);
-                    $megaArray[]=str_replace("thmb_","x16_",$img->src);
-
-                }
-            }
-            if(count($movies)>0){
-                foreach ($movies as $movie) {
-                    $movieArray[] = $movie->src;
-                    }
-            }
 
             self::$data = array(
                 "url" => $url,
                 "title" => $title->plaintext,
-                "breadCrumb" => $breadArray,
-                "address" => array("city" => trim($address[0]->plaintext),"town" => trim($address[1]->plaintext),"district" => trim($address[2]->plaintext)),
-                "price" => trim(str_replace($priceTrim->plaintext,"",$price->plaintext)),
-                "seller" => array("name"=>trim(@$sellerName->plaintext),"store_link" =>$sellerStore->href,"image"=>@$sellerImg->src,"phones" => @$phoneArray),
-                "coordinates" => array("latitude"=>trim(@$map->attr["data-lat"]),"longitude" =>trim(@$map->attr["data-lon"]) ),
-                "info" => $infoArray,
-                "properties" =>$propertyArray,
-                "description"=> $description->plaintext,
-                "description_base64"=> base64_encode($description->plaintext),
-                "thumbs" => $thumbArray,
-                "images" => $imageArray,
-                "mega_images"=> @$megaArray,
-                "movies"=>@$movieArray);
+                "breadCrumb" => self::getDetailBreadcrumb($open),
+                "address" => self::getDetailAddress($open),
+                "price" => self::getDetailPrice($open),
+                "seller" => self::getDetailSeller($open),
+                "coordinates" => self::getDetailCoordinates($open),
+                "info" => self::getDetailInfo($open),
+                "properties" => self::getDetailProperties($open),
+                "description" => self::getDetailDescription($open),
+                "media" => self::getDetailMedia($open)
+            );
 
 
-        }else{
-            self::$data[] = array("error" => true, "url" => $url,"message" => "Sonuç Bulunamadı.");
+        } else {
+            self::$data[] = array("error" => true, "url" => $url, "message" => "Sonuç Bulunamadı.");
         }
 
 
-            return self::ReturnWithTypes($type);
+        return self::ReturnWithTypes($type);
 
     }
 
@@ -290,8 +231,241 @@ class Sahibinden
     }
 
 
+    /**
+     * Detay methodu için ilanın video ve fotoğraflarını getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailMedia($html)
+    {
+        $images = str_get_html($html)->find("ul.classifiedDetailThumbListPages img");
+        $movies = str_get_html($html)->find("source#mp4");
 
-    static function ReturnWithTypes($type="json"){
+        $thumbArray = array();
+        $imageArray = array();
+        $megaArray = array();
+        $movieArray = array();
+        if (count($images) > 0) {
+            foreach ($images as $img) {
+                $thumbArray[] = $img->src;
+                $imageArray[] = str_replace("thmb_", "", $img->src);
+                $megaArray[] = str_replace("thmb_", "x16_", $img->src);
+
+            }
+        }
+        if (count($movies) > 0) {
+            foreach ($movies as $movie) {
+                $movieArray[] = $movie->src;
+            }
+        }
+
+
+        $return = array("thumb_images" => $thumbArray,
+            "standart_images" => $imageArray,
+            "mega_images" => $megaArray,
+            "movies" => $movieArray);
+
+        return $return;
+
+
+    }
+
+    /**
+     * Detay methodu için ilanın özelliklerini getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailProperties($html)
+    {
+        $propertyTitles = str_get_html($html)->find("div#classified-detail", 0)->find("div.uiBox", 1)->find("div.classifiedDescription", 0)->find("h3");
+        $propertyCount = str_get_html($html)->find("div#classified-detail", 0)->find("div.uiBox", 1)->find("div.classifiedDescription", 0)->find("ul");
+        $propertyArray = array();
+        if (count($propertyCount) > 0) {
+
+            for ($p = 0; $p <= count($propertyCount) - 1; $p++) {
+                $propertyDetails = str_get_html($html)->find("div#classified-detail", 0)->find("div.uiBox", 1)->find("div.classifiedDescription", 0)->find("ul", $p)->find("li.selected");
+                $ppDetails = array();
+                if (count($propertyDetails) > 0) {
+                    for ($d = 0; $d <= count($propertyDetails) - 1; $d++) {
+                        $ppDetails[] = trim($propertyDetails[$d]->plaintext);
+                    }
+                }
+
+                $propertyArray[] = array(trim($propertyTitles[$p]->plaintext) => $ppDetails);
+            }
+            return $propertyArray;
+        } else {
+            return $propertyArray;
+
+        }
+
+
+    }
+
+    /**
+     * Detay methodu için ilanın breadcrumb'ını getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailBreadcrumb($html)
+    {
+        $breadCrumb = str_get_html($html)->find("div.classifiedBreadCrumb ul li");
+        $breadArray = array();
+        if (count($breadCrumb) > 0) {
+            foreach ($breadCrumb as $bc) {
+                $breadArray[] = trim($bc->plaintext);
+            }
+        }
+
+        return $breadArray;
+
+
+    }
+
+
+    /**
+     * Detay methodu için ilanın adresini getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailAddress($html)
+    {
+        $address = str_get_html($html)->find("div.classifiedInfo h2", 0)->find("a");
+
+
+        $return = array(
+            "city" => trim($address[0]->plaintext),
+            "town" => trim($address[1]->plaintext),
+            "district" => trim($address[2]->plaintext)
+        );
+
+        return $return;
+
+    }
+
+
+    /**
+     * Detay methodu için ilanın koordinatlarını getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailCoordinates($html)
+    {
+
+        $map = str_get_html($html)->find("div#gmap", 0);
+        $return = array(
+            "latitude" => trim(@$map->attr["data-lat"]),
+            "longitude" => trim(@$map->attr["data-lon"])
+        );
+
+        return $return;
+
+
+    }
+
+
+    /**
+     * Detay methodu için ilanın fiyatını getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailPrice($html)
+    {
+
+        $price = str_get_html($html)->find("div.classifiedInfo h3", 0);
+        $priceTrim = str_get_html($html)->find("div.classifiedInfo h3 a", 0);
+
+
+        return trim(str_replace($priceTrim->plaintext, "", $price->plaintext));
+
+    }
+
+    /**
+     * Detay methodu için ilan sahibinin bilgilerini getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailSeller($html)
+    {
+
+        $sellerName = str_get_html($html)->find("div.classifiedUserContent h5", 0);
+        $sellerStore = str_get_html($html)->find("a.userClassifieds", 0);
+        $sellerImg = str_get_html($html)->find("div.classifiedUserContent a img", 0);
+        $sellerPhoneFields = str_get_html($html)->find("ul#phoneInfoPart li strong");
+        $sellerPhoneText = str_get_html($html)->find("ul#phoneInfoPart li span.pretty-phone-part");
+
+        if (count($sellerPhoneFields) > 0) {
+            for ($f = 0; $f <= count($sellerPhoneFields) - 1; $f++) {
+                $phoneArray[] = array("title" => trim($sellerPhoneFields[$f]->plaintext), "text" => trim($sellerPhoneText[$f]->plaintext));
+            }
+        }
+
+        $return = array(
+            "name" => trim(@$sellerName->plaintext),
+            "store_link" => $sellerStore->href,
+            "image" => @$sellerImg->src,
+            "phones" => @$phoneArray
+        );
+
+        return $return;
+
+    }
+
+    /**
+     * Detay methodu için ilanın açıklamasını getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailDescription($html)
+    {
+
+        $description = str_get_html($html)->find("div.classifiedDescription", 0);
+
+        $return = array("text" => trim($description->plaintext),
+            "base64" => base64_encode($description->innertext));
+
+        return $return;
+    }
+
+    /**
+     * Detay methodu için ilanın detaylarını getirir
+     *
+     * @param $html //Detay sayfası html
+     * @return  Array
+     */
+    private function getDetailInfo($html)
+    {
+
+        $infoListFields = str_get_html($html)->find("ul.classifiedInfoList li strong");
+        $infoListTexts = str_get_html($html)->find("ul.classifiedInfoList li span");
+        $infoArray = array();
+        if (count($infoListFields) > 0) {
+            for ($f = 0; $f <= count($infoListFields) - 1; $f++) {
+                $infoArray[] = array("title" => trim($infoListFields[$f]->plaintext), "text" => trim($infoListTexts[$f]->plaintext));
+            }
+        }
+
+        return $infoArray;
+
+
+    }
+
+    /**
+     * Array formatından XML veya JSON formatı oluşturur
+     *
+     * @param $type //Dönecek veri formatı
+     * @return  JSON,XML,Array
+     */
+    private function ReturnWithTypes($type = "json")
+    {
 
         if ($type == "json" or empty($type)) {
             return json_encode(self::$data);
@@ -303,6 +477,7 @@ class Sahibinden
             return $xml->asXML();
         }
     }
+
     /**
      * Gereksiz boşlukları temizler.
      *
